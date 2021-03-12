b4ab698b2874153939f8f9d439963e8d7c8b10bf
theJonson <me@thejonson.com>
2021-03-12 16:24:03 +0000

    Create scraper.sh

---
+++ scraper.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#create working folder
+WORK=/tmp/shutterstock
+if [ ! -d "$WORK" ]; then
+  mkdir $WORK
+fi
+
+#Move to the WORK directory
+cd $WORK
+
+#Build date variables
+echo "Building Dates"
+TODAY=`date +%F`
+TIME=`date +%T`
+
+echo $TODAY
+echo $TIME
+
+#Build start and end date variables
+echo "Building Start and End"
+START=date_updated_start=$TODAY\T00:00:01Z
+END=date_updated_end=$TODAY\T$TIME\Z
+
+echo $START
+echo $END
+
+#Set API KEY
+KEY="v2/TTRiN1M3ZFR6SHNic2QzWXZEcGxRM2J4a3FTNzNLWmEvMjk0MTg5ODkxL2N1c3RvbWVyLzQvZ3h4MG41ZlZPRGVVdHktTmFOU1FsSUEwV01OWnZMSDgwSlMtUTNCN1Rab1VFeG83eVNfZVJTdktfWFNsN09lQ1dsSDktcVVZNUEzQXl5MUVoNzJFLU84N0RxVDA1b2tKZlJicnVWLXJFSmFhd3o0aXdxWUtoMGlkRUVVTV81S3ZkOW9sUi1ocFpJV1NlNWFHTFRMVWk4U0czeHl6UkFGZ0lKV0tIbG9FRDVZQXRTcm9XWTV6WEk4ZXkzWHJpLUxVUFlQYXBFUFlYbkFzRlFXUmxVUTd4QS9tRTQza3FScl8tcGdscWR3OFE0dmh3"
+API_KEY="Authorization: Bearer $KEY"
+
+#Remove old image_ids.txt
+echo "Remove old image ids"
+rm -f $WORK/image_ids.txt
+
+#Gets all images uploaded between 2 dates
+echo "Find images"
+
+curl -s -X GET https://api.shutterstock.com/v2/editorial/images/updated -H "Accept: application/json" -G -H "$API_KEY" --data-urlencode "type=addition" --data-urlencode "country=GBR" --data-urlencode "supplier_code=SPTQ" --data-urlencode "date_updated_start=2021-03-09T00:00:01Z" --data-urlencode "date_updated_end=2021-03-09T23:59:59Z" | jq -r '.data[] | .id' > $WORK/image_ids.txt
+
+#curl -s -X GET https://api.shutterstock.com/v2/editorial/images/updated -H "Accept: application/json" -G -H "$API_KEY" --data-urlencode "type=addition" --data-urlencode "country=GBR" --data-urlencode "supplier_code=SPTQ" --data-urlencode "$START" --data-urlencode "$END" | jq -r '.data[] | .id' > $WORK/image_ids.txt
+
+#Check if there are any image ids
+if [ -f $WORK/image_ids.txt ]
+then
+    if [ -s $WORK/image_ids.txt ]
+    then
+        echo "Image IDs found in image_ids.txt"
+    else
+        echo "No Image IDs found in image_ids.txt"
+        exit 1
+    fi
+else
+    echo "Couldn't find image_ids.txt"
+    exit 1
+fi
+
+#For each ID listed in image_ids license the image and output the result to json file
+echo "License the images"
+while IFS= read -r line
+do
+  curl -s -X POST "https://api.shutterstock.com/v2/editorial/images/licenses" -H "accept: application/json" -H "Content-Type: application/json" -H "$API_KEY" -d "{\"editorial\":[{\"editorial_id\":\"$line\",\"license\":\"premier_editorial_comp\",\"metadata\":{\"purchase_order\":\"-\"}}],\"country\":\"GBR\"}" > $WORK/$line.json
+done < $WORK/image_ids.txt
+
+#Remove existing json file just in case
+rm -f $WORK/combined.json
+
+#Combine all the individual image json responses into a single one
+jq '.' $WORK/*.json > $WORK/combined.json
+
+#Remove existing url file just in case
+rm -f $WORK/urls.txt
+
+#Filter the combined json to just get the download urls into a txt file
+cat $WORK/combined.json | jq -r '.data[] | .download.url' > $WORK/urls.txt
+
+#Pass the urls to curl to download the file -J is needed to output the .jpg file, otherwise the file name is the request url
+#The cd at the top is really only needed for this command as curl doesn't have a folder path switch
+xargs -n 1 curl -s -O -J -L < $WORK/urls.txt
+
+#Clean up all the json files as we don't need them anymore
+rm -f $WORK/*.json
+
+#Upload files to S3 bucket - setup s3cmd first
+s3cmd sync $WORK/*.jpg s3://qpr-greenfly --skip-existing --quiet
+
+#Remove any downloaded jpgs and txt files
+rm -f $WORK/*.jpg
+rm -f $WORK/*.txt
